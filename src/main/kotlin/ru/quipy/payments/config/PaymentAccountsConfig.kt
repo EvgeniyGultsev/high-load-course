8c4d3e596c525450f907e9daf55ecdbd161489f8
polikiss <mymmypolly@gmail.com>
2025-11-20 19:05:00 +0300

    web client

--- src/main/kotlin/ru/quipy/payments/config/PaymentAccountsConfig.kt
+++ src/main/kotlin/ru/quipy/payments/config/PaymentAccountsConfig.kt
@@ -3,18 +3,25 @@ package ru.quipy.payments.config
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import io.netty.channel.ChannelOption
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.client.reactive.ReactorClientHttpConnector
 import org.springframework.web.reactive.function.client.WebClient
+import reactor.netty.resources.ConnectionProvider
+import reactor.netty.http.client.HttpClient
+import io.netty.handler.timeout.ReadTimeoutHandler
+import io.netty.handler.timeout.WriteTimeoutHandler
 import ru.quipy.core.EventSourcingService
 import ru.quipy.metrics.MetricsCollector
 import ru.quipy.payments.api.PaymentAggregate
 import ru.quipy.payments.logic.*
 import java.net.URI
-import java.net.http.HttpClient
+import java.net.http.HttpClient as JavaHttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
+import java.time.Duration
 import java.util.*
 
 
@@ -23,7 +30,7 @@ class PaymentAccountsConfig(
     private val metricsCollector: MetricsCollector
 ) {
     companion object {
-        private val javaClient = HttpClient.newBuilder().build()
+        private val javaClient = JavaHttpClient.newBuilder().build()
         private val mapper = ObjectMapper().registerKotlinModule().registerModules(JavaTimeModule())
     }
 
@@ -38,6 +45,31 @@ class PaymentAccountsConfig(
 
     @Value("#{'\${payment.accounts}'.split(',')}")
     lateinit var allowedAccounts: List<String>
+
+    @Bean
+    fun webClient(): WebClient {
+        val connectionProvider = ConnectionProvider.builder("payment-client")
+            .maxConnections(500)
+            .maxIdleTime(Duration.ofSeconds(20))
+            .maxLifeTime(Duration.ofMinutes(10))
+            .pendingAcquireTimeout(Duration.ofSeconds(60))
+            .evictInBackground(Duration.ofSeconds(120))
+            .build()
+        
+        val httpClient = HttpClient.create(connectionProvider)
+            .responseTimeout(Duration.ofSeconds(300))
+            .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 60000)
+            .option(ChannelOption.SO_KEEPALIVE, true)
+            .option(ChannelOption.TCP_NODELAY, true)
+            .doOnConnected { conn ->
+                conn.addHandlerLast(ReadTimeoutHandler(300))
+                conn.addHandlerLast(WriteTimeoutHandler(60))
+            }
+        
+        return WebClient.builder()
+            .clientConnector(ReactorClientHttpConnector(httpClient))
+            .build()
+    }
 
     @Bean
     fun accountAdapters(
